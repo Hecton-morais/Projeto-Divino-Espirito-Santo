18020996a1c824cac0bec4d26edb68116b6b27d7
Hecton <lopchato@gmail.com>
2026-05-16 00:20:08 -0300

    subindo site completo

---
+++ src/script.js
@@ -0,0 +1,41 @@
+const sections = document.querySelectorAll('section');
+
+function revealSections() {
+
+  sections.forEach(section => {
+
+    const sectionTop = section.getBoundingClientRect().top;
+
+    const triggerPoint = window.innerHeight - 100;
+
+    if(sectionTop < triggerPoint) {
+      section.classList.add('show');
+    }
+
+  });
+
+}
+
+window.addEventListener('scroll', revealSections);
+
+revealSections();
+const track = document.querySelector('.gallery-track');
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+
+const imageWidth = 320;
+
+nextBtn.addEventListener('click', () => {
+  track.scrollTo({
+    left: track.scrollLeft + imageWidth,
+    behavior: 'smooth'
+  });
+});
+
+prevBtn.addEventListener('click', () => {
+  track.scrollTo({
+    left: track.scrollLeft - imageWidth,
+    behavior: 'smooth'
+  });
+});
+
